895dafab9af0eb530fc68b90b057fbbcd9614938
Lex <lex.bay93@gmail.com>
2020-01-15 18:45:08 -0600

    added ability to add a movie

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit , Input} from '@angular/core';
 import {Movie}  from "../movie"
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-movies-list',
@@ -11,11 +12,20 @@ export class MoviesListComponent implements OnInit {
   @Input() movies:Movie[];
   @Input() name:string;
 
+  newTitle: string;
+  newCategory: string;
+  newYear: number;
+
   constructor() { 
   }
 
   details(movie:Movie){
     alert(`details for ${movie.title}`);
+  }
+
+  addMovie(){
+    let movie = new Movie(this.newTitle, this.newCategory, this.newYear);
+    this.movies.push(movie);
   }
 
   ngOnInit() {
